155520d28fd86a3569daa44b2b0910648a23864b
muktiwrtm <50369374+muktiwrtm@users.noreply.github.com>
2020-04-15 13:29:59 +0700

    Add files via upload

---
+++ crud/src/MainClass.java
@@ -0,0 +1,11 @@
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        formmhs fm = new formmhs();
+        fm.tesformmhs();
+    }
+
+}
